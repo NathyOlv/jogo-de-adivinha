b1ac2dc8f8aad740fdaa0edbb39a5153719a4741
NathyOlv <natasha.olv@hotmail.com>
2019-02-11 20:19:57 -0200

    Primeira versão do jogo

---
+++ jogo_adivinha_v1-1.rb
@@ -0,0 +1,13 @@
+#Exercício 1 de Ruby
+#Jogo de Adivinha v1.1
+
+  puts "Informe seu nome"
+  nome_usuario = gets
+  
+  puts "Informe sua tentativa"
+  valor_usuario = gets
+  
+  valor_programador = 66
+  
+  puts nome_usuario
+  puts valor_programador == valor_usuario.to_i
