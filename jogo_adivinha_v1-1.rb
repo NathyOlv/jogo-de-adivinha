e09dad01f23b5d2208300cddd35d1434ffbde250
NathyOlv <natasha.olv@hotmail.com>
2019-02-18 20:07:47 -0300

    #1: Tres tentativas implementada.

--- jogo_adivinha_v1-1.rb
+++ jogo_adivinha_v1-1.rb
@@ -1,13 +1,48 @@
 #Exercício 1 de Ruby
 #Jogo de Adivinha v1.1
 
-  puts "Informe seu nome"
-  nome_usuario = gets
-  
-  puts "Informe sua tentativa"
-  valor_usuario = gets
-  
   valor_programador = 66
+    
+    puts "Informe seu nome"
+    nome_usuario = gets
   
-  puts nome_usuario
-  puts valor_programador == valor_usuario.to_i
+
+    puts "\nPrimeira tentativa"
+    valor_usuario = gets
+  
+    puts nome_usuario
+    puts valor_programador == valor_usuario.to_i
+    if valor_programador > valor_usuario.to_i
+      puts "O valor informado é menor que o secreto"
+    elsif valor_programador < valor_usuario.to_i
+      puts "O valor informado é maior que o secreto"
+    else
+      puts "Você acertou! Parabéns!"
+    end
+      
+    puts "\nSegunda tentativa"
+    valor_usuario = gets
+  
+    puts nome_usuario
+    puts valor_programador == valor_usuario.to_i
+    if valor_programador > valor_usuario.to_i
+      puts "O valor informado é menor que o secreto"
+    elsif valor_programador < valor_usuario.to_i
+      puts "O valor informado é maior que o secreto"
+    else
+      puts "Você acertou! Parabéns!"
+    end
+  
+    puts "\nTerceira tentativa"
+    valor_usuario = gets
+  
+    puts nome_usuario
+    puts valor_programador == valor_usuario.to_i
+    if valor_programador > valor_usuario.to_i
+      puts "O valor informado é menor que o secreto"
+    elsif valor_programador < valor_usuario.to_i
+      puts "O valor informado é maior que o secreto"
+    else
+      puts "Você acertou! Parabéns!"
+    end
+
